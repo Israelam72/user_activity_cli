096a2930d6b88ff5b4cfd81b65c9fa65d61208d7
Israel Arantes <israelarantes.m@gmail.com>
2024-11-10 21:45:18 -0300

    + some adustments

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ from typing_extensions import Annotated
 
 
 def main():
-    username: Annotated[str, typer.Argument(help="The user name to get activity from")] = typer.prompt("Username")
-    headers = {'Accept': 'application/json'}
-    end_point = f"https://api.github.com/users/{username}/events"
-    
-    test = requests.get(end_point, headers=headers)
-    print(test.json())
+    while True:
+        username: Annotated[str, typer.Argument(help="The user name to get activity from")] = typer.prompt("Username")
+        headers = {'Accept': 'application/json'}
+        end_point = f"https://api.github.com/users/{username}/events"
+
+        
+        response = requests.get(end_point, headers=headers)
+
+        if response.status_code == 404:
+            print('User not found')
+        else:
+            print(response.json())
+            break
 
 if __name__ == "__main__":
     typer.run(main)
@@ -19,4 +26,5 @@ if __name__ == "__main__":
 - Verificar como o json está chegando
 - arrumar o json pra ficar legivel
 - estudar para ver se é possivel adicionar class
+- verificar o --help para descrever o 'username'
 """
--- utils/arg_parser.py
+++
@@ -1,6 +0,0 @@
-import argparse
-
-def create_parser():
-    parser = argparse.ArgumentParser(prog='github-activity', description='Simple CLI to get Git-hub user activity')
-    parser.add_argument('github-activity', help='Get a Git-hub user activity')
-    return parser
