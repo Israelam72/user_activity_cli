87ce1caed9f09590ea0e9421da8acf8e7fa7ffbc
Israel Arantes <israelarantes.m@gmail.com>
2024-11-11 22:38:38 -0300

    + final commit, json fixed, user -help fixed

--- main.py
+++ main.py
@@ -4,27 +4,31 @@ import requests
 from typing_extensions import Annotated
 
 
-def main():
-    while True:
-        username: Annotated[str, typer.Argument(help="The user name to get activity from")] = typer.prompt("Username")
-        headers = {'Accept': 'application/json'}
-        end_point = f"https://api.github.com/users/{username}/events"
+def main(
+    username: Annotated[str, typer.Argument(help="The user name to get activity from")]
+):
+    headers = {'Accept': 'application/vnd.github+json'}
+    end_point = f"https://api.github.com/users/{username}/events"
 
-        
-        response = requests.get(end_point, headers=headers)
+    
+    response = requests.get(end_point, headers=headers)
 
-        if response.status_code == 404:
-            print('User not found')
+    if response.status_code == 404:
+        print('User not found')
+    else:
+        events = response.json()
+        if not events:
+            print('No recent activity found for this user')
         else:
-            print(response.json())
-            break
+            for event in events:
+                repo_name = event.get('repo',{}).get('name','No repo')
+                event_type = event.get('type', 'No type')
+                created_at = event.get('created_at','No date')
+
+                print(f"\nRepository: {repo_name}")
+                print(f"Event Type: {event_type}")
+                print(f"Date: {created_at}")
 
 if __name__ == "__main__":
     typer.run(main)
-
-"""
-- Verificar como o json está chegando
-- arrumar o json pra ficar legivel
-- estudar para ver se é possivel adicionar class
-- verificar o --help para descrever o 'username'
-"""
+    
